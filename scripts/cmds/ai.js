3e354faf47a044018deae39ad1f4c6526afe886a
Ai37-gif <angesk37@gmail.com>
2025-05-02 07:50:24 +0100

    ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -2,7 +2,7 @@
 const UPoLPrefix = [
   'edu',
   'ai',
-  'Megan',
+  'akashi',
   'bot',
   'ask'
 ]; 
@@ -28,11 +28,11 @@ const UPoLPrefix = [
       
      const upol = event.body.substring(ahprefix.length).trim();
    if (!upol) {
-        await message.reply('Empereur Ciel ✨\n━━━━━━━━━━━━━\nAsk Me Your Question');
+        await message.reply('☬𝐄𝐌𝐏𝐄𝐑𝐄𝐔𝐑 𝐀𝐊𝐀𝐒𝐇𝐈☬ 🌿\n━━━━━━━━━━━━━\nAsk Me Your Question');
         return;
       }
       
-      const apply = ['Creature inférieur que veux tu' , maybe you need my help', 'How can i help you?', 'How can i assist you today?', 'How can i help you?🙂'];
+      const apply = ['maybe you need my help', 'How can i help you?', 'How can i assist you today?', 'How can i help you?🙂'];
       
      const randomapply = apply[Math.floor(Math.random() * apply.length)];
 
